e426c7aedc88e1728f520f70cac2ee6ab234a8e4
= <=>
2018-08-15 14:03:10 -0500

    crear y mostrar items en la toolbar

--- app/src/main/java/com/maycc/mycontacts/MainActivity.kt
+++ app/src/main/java/com/maycc/mycontacts/MainActivity.kt
@@ -2,11 +2,21 @@ package com.maycc.mycontacts
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import kotlinx.android.synthetic.main.toolbar_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(mainToolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+
+        return super.onCreateOptionsMenu(menu)
     }
 }
