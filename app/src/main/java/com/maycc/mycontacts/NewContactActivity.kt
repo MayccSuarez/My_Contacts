ffb3d2f71d3ec480520f1620602d7be89e705c58
= <=>
2018-08-17 10:10:03 -0500

    mostar item guardar y botón de navegación hacia atrás

--- app/src/main/java/com/maycc/mycontacts/NewContactActivity.kt
+++ app/src/main/java/com/maycc/mycontacts/NewContactActivity.kt
@@ -2,11 +2,31 @@ package com.maycc.mycontacts
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_new_contact.*
 
 class NewContactActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_contact)
+
+        showBackButton()
+    }
+
+    private fun showBackButton() {
+        setSupportActionBar(toolbarNewContact)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_new_contact, menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return super.onOptionsItemSelected(item)
     }
 }
