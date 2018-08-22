5c7d35e874c0b8c7ce3186fa0a4f7a864743e622
= <=>
2018-08-22 12:31:01 -0500

    Agregar propiedad id al contacto
    Esto nos permitirá hacer referencia a un contacto en específico

--- app/src/main/java/com/maycc/mycontacts/Contact.kt
+++ app/src/main/java/com/maycc/mycontacts/Contact.kt
@@ -2,4 +2,4 @@ package com.maycc.mycontacts
 
 import java.io.Serializable
 
-class Contact (var name :String, var lastName :String, var phone :String, var email :String, var img :Int) : Serializable
+class Contact (var id :Int, var name :String, var lastName :String, var phone :String, var email :String, var img :Int) : Serializable
